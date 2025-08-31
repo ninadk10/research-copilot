063d761b6123aa3f30c6db7de213b491c60e9313
Ninad Kothmire <ninadk1012@gmail.com>
2025-08-31 17:54:58 +0100

    completed ingest.py

--- ingest.py
+++ ingest.py
@@ -16,10 +16,12 @@ store_embeddings(chunks, embeddings)
 
 from PyPDF2 import PdfReader
 import os
+from sentence_transformers import SentenceTransformer
+import chromadb
 
 
-
-folder_path = os.fsencode("")
+client = chromadb.PersistentClient()
+folder_path = os.fsencode("data/")
 
 def load_pdfs(folder_path):
     all_texts = []
@@ -47,11 +49,36 @@ def chunk_text(text, chunk_size=500, overlap = 50):
     return chunks
 
 
-
-
-
-def embed_chunks(chunks):
+def embed_chunks(chunks, model_name):
     # sentence transformers for embeddings
+    model = SentenceTransformer(model_name) 
+    embeddings = []
+    for chunk in chunks:
+        embedding = model.encode(chunk)
+        embeddings.append(embedding)
+    return embeddings
 
-def store_embeddings(chunks,embeddings):
+def store_embeddings(chunks,embeddings, db_path="./vector_store", collection_name="research_docs"):
     # chroma db for vector store
+    collection = client.get_or_create_collection(name=collection_name)
+    ids = [f"doc_{i}" for i in range(len(chunks))]
+
+    collection.add(
+        ids=ids,
+        documents = chunks,
+        embeddings = embeddings
+    )
+
+    print(f"Stored {len(chunks)} chunks in ChromaDB at {db_path}")
+
+
+# ---- main code ------
+texts = load_pdfs(folder_path)
+
+all_chunks = []
+for text in texts:
+    chunks = chunk_text(text)
+    all_chunks.append(chunks)
+
+embeddings = embed_chunks(all_chunks, model_name="all-MiniLM-L6-v2")
+store_embeddings(all_chunks,embeddings,)

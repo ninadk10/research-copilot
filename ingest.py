738fc6059c23bd3e67351bfff9ddbf4f7936653b
Ninad Kothmire <ninadk1012@gmail.com>
2025-09-09 08:47:47 +0100

    ingest.py dependency issues fixed, code now working for a single topic

--- ingest.py
+++ ingest.py
@@ -2,13 +2,14 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 import feedparser
+import urllib.parse
 import requests
 from pathlib import Path
 
-DATA_PATH = "data"
+DATA_DIR = "data"
 DB_PATH = "./vector_store"
 
 # search arXiv for relevant papers
@@ -17,18 +18,27 @@ def arxiv_search(query, max_results=5):
     feed = feedparser.parse(url)
     results = []
     for entry in feed.entries:
-        results.append({
-            "title": entry.title,
-            "summary": entry.summary,
-            "pdf_url": entry.links[1].href,  # link[0] is abstract, link[1] is PDF
-            "authors": [a.name for a in entry.authors],
-            "published": entry.published
-        })
+        pdf_url = None
+        #  Iterate through the links to find pdf link with title "pdf"
+        for link in entry.links:
+            if link.get("title") == "pdf":
+                pdf_url = link.href
+                break
+        if pdf_url:
+            results.append({
+                "title": entry.title,
+                "summary": entry.summary,
+                "pdf_url": pdf_url,
+                "authors": [a.name for a in entry.authors],
+                "published": entry.published
+            })
+        else:
+            print(f"PDF link not found for entry: {entry.title}")
     return results
 
 
 # fetch and downlaod pdfs locally
-def fetch_pdf(url, out_dir="data/papers"):
+def fetch_pdf(url, out_dir=DATA_DIR):
     out_path = Path(out_dir)
     out_path.mkdir(parents=True, exist_ok=True)
     filename = out_path / (url.split("/")[-1] + ".pdf")
@@ -39,12 +49,13 @@ def fetch_pdf(url, out_dir="data/papers"):
     return str(filename)
 
 # parse and chunk text
-def load_and_split_pdfs(pdf_dir):
+def parse_and_chunk(filename):
     """Load all PDFs from directory and split into chunks."""
     documents = []
-    for file in os.listdir(pdf_dir):
-        if file.endswith(".pdf"):
-            loader = PyPDFLoader(os.path.join(pdf_dir, file))
+    for filename in os.listdir(DATA_DIR):
+        pdf_path = os.path.join(DATA_DIR, filename)
+        if pdf_path.endswith(".pdf"):
+            loader = PyPDFLoader(os.path.join(DATA_DIR, filename))
             documents.extend(loader.load())
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
@@ -60,13 +71,14 @@ def store_embeddings(chunks, db_path=DB_PATH):
 
 # main ingest function
 def ingest_topic(query, max_results=5):
-    results = arxiv_search(query, max_results=max_results)
+    clean_query = urllib.parse.quote_plus(query)
+    results = arxiv_search(clean_query, max_results=max_results)
     for paper in results:
         pdf_path = fetch_pdf(paper["pdf_url"])
+        print(pdf_path)
         chunks = parse_and_chunk(pdf_path)
         store_embeddings(chunks)
     print(f"Ingested {len(results)} papers for topic: {query}")
 
 
-
-
+ingest_topic("LLM")
